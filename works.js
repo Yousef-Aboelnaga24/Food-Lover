2abb6325d3597ddd05e13373db3e552125623b87
Yousef Walid <aboelnaga0200824@gmail.com>
2025-08-12 00:40:06 +0300

    Delete works.js

--- works.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    let header = document.getElementById('navegation')
-    let top = window.scrollY
-    if (top > 1.5) {
-        header.classList.add('scrolled')
-    } else {
-        header.classList.remove('scrolled')
-    }
-})
